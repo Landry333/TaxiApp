9d37f11979ffa1ede4b65a2b112072474339d72f
landry333 <57639988+Landry333@users.noreply.github.com>
2020-08-12 23:59:15 -0400

    Customer action OnClickListener aded
    Java code for action on choosing user as customer added

--- app/src/main/java/com/example/taxiapp/MainActivity.java
+++ app/src/main/java/com/example/taxiapp/MainActivity.java
@@ -26,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         });
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
 
     }
 }
